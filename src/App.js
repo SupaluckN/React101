6c7be37a0b0b545b4bae6fb943d3694fb0d2afe7
SupaluckN <supaluckingjan@gmail.com>
2022-09-18 21:19:40 +0700

    React Hook -- useState: onClick: textColor

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { useState } from 'react'
 
 function App() {
 
-  const [showText, setShowText] = useState(true);
+  const [textColor, setTextColor] = useState("black");
 
-  const toggleText = () => {
-    setShowText(!showText)
-  }
+  
 
   return (
     <div className="App">
-      <button onClick={toggleText}>Show/Hide</button>
-      {showText === true && <h1>Hi my name is Nanny</h1>}
+      <button onClick={() => {
+        setTextColor(textColor === "black" ? "red": "black")
+      }}>Show/Hide</button>
+      <h1 style={{color: textColor}}>Hi my name is Nanny</h1>
     </div>
   );
 }
